8578bd2bace03834fe706b582e79abdb6e1b8d67
Carlos Gutierrez <cgcarlosg@hotmail.com>
2025-07-21 08:13:44 -0500

    Finish testing drawingManager

--- src/components/MapDashboard/MapDashboard.tsx
+++ src/components/MapDashboard/MapDashboard.tsx
@@ -1,33 +1,84 @@
 // src/components/MapDashboard.tsx
-import React from "react";
-import { useLoadScript } from "@react-google-maps/api";
-import { GoogleMap } from "@react-google-maps/api";
+import React, { useCallback, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  DrawingManager,
+} from "@react-google-maps/api";
 
-import './MapDashboard.scss';
+import "./MapDashboard.scss";
 
 const center = {
   lat: 4.710989,
-  lng: -74.072090,
+  lng: -74.07209,
 };
 
 const MapDashboard: React.FC = () => {
+  const [shapes, setShapes] = useState<
+    Array<
+      | google.maps.Circle
+      | google.maps.Polygon
+      | google.maps.Polyline
+      | google.maps.Rectangle
+    >
+  >([]);
+
+  const onOverlayComplete = useCallback(
+    (e: google.maps.drawing.OverlayCompleteEvent) => {
+      const newShape = e.overlay;
+      const type = e.type;
+
+      if (type === "marker") {
+        console.log("Se ignoró un marcador");
+        return;
+      }
+
+      setShapes((prev) => [
+        ...prev,
+        newShape as
+          | google.maps.Circle
+          | google.maps.Polygon
+          | google.maps.Polyline
+          | google.maps.Rectangle,
+      ]);
+
+      console.log("Dibujo completo:", type, newShape);
+    },
+    []
+  );
+
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    libraries: [],
+    libraries: ["drawing"],
   });
-// to handle errors during charging
+
+  // to handle errors during charging
   if (loadError) return <div>Error cargando el mapa</div>;
   if (!isLoaded) return <div>Cargando mapa...</div>;
 
   return (
     <div className={"dashboard"}>
       <h2>Dashboard de Mapa</h2>
-       <GoogleMap
+      <GoogleMap
         mapContainerClassName="dashboard__container"
         center={center}
         zoom={12}
       >
-        {/* To Add Logic */}
+        <DrawingManager
+          onOverlayComplete={onOverlayComplete}
+          options={{
+            drawingControl: true,
+            drawingControlOptions: {
+              position: window.google.maps.ControlPosition.TOP_CENTER,
+              drawingModes: [
+                "polygon",
+                "rectangle",
+                "circle",
+                "polyline",
+              ] as google.maps.drawing.OverlayType[],
+            },
+          }}
+        />
       </GoogleMap>
     </div>
   );
