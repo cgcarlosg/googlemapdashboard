959997df5a64960716efda928b39a8517586ea27
Carlos Gutierrez <cgcarlosg@hotmail.com>
2025-07-20 22:27:24 -0500

    mapcomponent added

---
+++ src/components/MapDashboard.tsx
@@ -0,0 +1,16 @@
+// src/components/MapDashboard.tsx
+import React from 'react';
+
+const MapDashboard: React.FC = () => {
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h2>Dashboard de Mapa</h2>
+      <div
+      >
+        Mapa
+      </div>
+    </div>
+  );
+};
+
+export default MapDashboard;
