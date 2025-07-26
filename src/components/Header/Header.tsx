20e193cb8249d6d87290be16793d9b9aa7735685
Carlos Gutierrez <cgcarlosg@hotmail.com>
2025-07-26 08:26:37 -0500

    add mock locations to be used in autocomplete search box

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import "./Header.scss";
+
+type Props = {
+  onLocationSelect: (coords: google.maps.LatLngLiteral) => void;
+};
+
+const mockLocations = [
+  { name: "Bogotá Centro", coords: {lat: 4.710989, lng: -74.07209 } },
+  { name: "Bogotá Aeropuerto", coords: { lat: 4.7010, lng: -74.1464 } },
+  { name: "Centro Comercial Multiplaza", coords: { lat: 4.6517, lng: -74.1265 } },
+  { name: "Centro Comercial Plaza Imperial", coords: { lat: 4.7496, lng: -74.0956 } },
+];
+
+const Header: React.FC<Props> = () => {
+
+  return (
+ <header className="header">
+      
+    </header>
+  );
+};
+
+export default Header;
