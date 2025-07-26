673a450071ef18e573ae29e6b69f455e5d9d1376
Carlos Gutierrez <cgcarlosg@hotmail.com>
2025-07-26 18:40:40 -0500

    localstorage implemented in order to maintain last coord when refreshing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MapContainer from './components/MapContainer/MapContainer';
 
 import './App.css'
 import Header from './components/Header/Header';
 
 const defaultCenter = {lat: 4.6517, lng: -74.1265 };
+const saved = localStorage.getItem("last-center");
+const initialCenter = saved ? JSON.parse(saved) : defaultCenter;
 
 const App = () => {
 
-    const [center, setCenter] = useState(defaultCenter);
+    const [center, setCenter] = useState(initialCenter);
+
+    useEffect(() => {
+    localStorage.setItem("last-center", JSON.stringify(center));
+  }, [center]);
 
   return (
     <div className="app">
