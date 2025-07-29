b28d6b37342548351c51f831de7616915301f8af
Carlos Gutierrez <cgcarlosg@hotmail.com>
2025-07-28 22:55:31 -0500

    remove extra text

--- src/components/OnboardingPopup/OnboardingPopup.tsx
+++ src/components/OnboardingPopup/OnboardingPopup.tsx
@@ -15,19 +15,19 @@ const OnboardingPopup: React.FC<OnboardingPopupProps> = ({ onClose }) => {
         <p>Para empezar, te explicamos cómo usar la aplicación:</p>
         <ol>
           <li>
-            **Buscar una ubicación:** Usa la **barra de búsqueda en la parte superior** para encontrar una dirección y centrar el mapa al instante. (todas las opciones actuales tiene la letra C para visualizarlas)
+            Buscar una ubicación: Usa la barra de búsqueda en la parte superior para encontrar una dirección y centrar el mapa al instante. (todas las opciones actuales tiene la letra C para visualizarlas)
           </li>
           <li>
-            **Definir tu ruta:** Alternativamente, **haz clic en cualquier lugar del mapa** para colocar tu **punto de inicio**. Vuelve a hacer clic para establecer tu **destino** y generar una ruta.
+            Definir tu ruta: Alternativamente, haz clic en cualquier lugar del mapa para colocar tu punto de inicio. Vuelve a hacer clic para establecer tu destino y generar una ruta.
           </li>
           <li>
-            **Ver detalles de hitos:** Haz clic en los **marcadores rojos (hitos)** en la ruta para ver información detallada y una vista de Google Street View.
+            Ver detalles de hitos: Haz clic en los marcadores rojos (hitos) en la ruta para ver información detallada y una vista de Google Street View.
           </li>
           <li>
-            **Controles del mapa:** Usa los **botones de control** en el mapa para alternar la visibilidad de hitos, círculos de influencia y puntos de interés.
+            Controles del mapa: Usa los botones de control en el mapa para alternar la visibilidad de hitos, círculos de influencia y puntos de interés.
           </li>
           <li>
-            **Limpiar datos:** Si deseas empezar de nuevo, usa el botón "Limpiar Datos" en los controles del mapa.
+            Limpiar datos: Si deseas empezar de nuevo, usa el botón "Limpiar Datos" en los controles del mapa.
           </li>
         </ol>
         <button onClick={onClose} className="onboarding__popup--button">¡Entendido!</button>
